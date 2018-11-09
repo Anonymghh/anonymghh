71486d55d1ccabadcb3980111618dff6013f85bd
Anonymghh <44904847+Anonymghh@users.noreply.github.com>
2018-11-09 20:14:00 +0300

    AnonymGhh
    AnonymGhh

---
+++ anonymghh.bat
@@ -0,0 +1,36 @@
+@echo off
+color 0a
+tittle anonymghh
+echo anonymghh
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+echo                              ..              ``..                .-       `.                       
+echo                            /dNN/ .os` /dm/`+dNNNNo  +y- -yms+d+ oNN./dm+.yNN/                      
+echo                          `yMMMMs`mMMo/MMN-hMMMdMMM`yMMd.mMM/dMNhMm:+MMMyNMMM.                      
+echo                         .mMNhMM:yMMMNNMM/hMMN:sMMm/MMMMmMMs hMMMd.-NMMMMMMMd                       
+echo                        .mMM+mMN:MMNMMMMs:MMN-`mMM+mMMNMMMm` oMMh` hMMMMMMMM+                       
+echo                       `dMMNhMMsyMN:mMMd`/MMs.hMN+/MMosMMN- /NMd` :MMN+y+MMN`                       
+echo                       -mm+.-mm-hh: /hs. `smmdmy- od+ .hy: .mMN+  /dd:  .dh:                        
+echo                        ..   `` `          `...    `        .-.`   ``    `                          
+echo                                         .:/+:`  .+o.`+s:`/o/ :so                                   
+echo                                       :hNMMMMd .NMm`sMM:yMM/.NMy                                   
+echo                                      sNMNhodmh`dMM/:NMs+MMh-yMm`                                   
+echo                                     +MMm-/ohh.oMMMmMMm-NMMNNMM/                                    
+echo                                     mMM+`+dMM+NMN/sMM+hMMs/NMm                                     
+echo                                     hMMNddNm+yMM:.NMm:MMh yMM/                                     
+echo                                     `+syso:` sh+ :hs-:hs. hh/                                      
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+
+pause > nul
